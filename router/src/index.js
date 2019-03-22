51e989e7ba65e07bbd1ac55729181e7212b95bca
wangjinshen <807253330@qq.com>
2019-03-22 20:34:11 +0800

    删点东西

--- router/src/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './contentais/app';
-import "./common/css/index.css"
-ReactDOM.render(<App />, document.getElementById('root'));
